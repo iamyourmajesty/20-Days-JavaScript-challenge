e4e9f64eaf1faee29b9998d86966cc1ac4de91e2
Prince Pandey <princepandey2784@gmail.com>
2023-02-13 22:30:58 +0530

    day 10

--- Day 10 - BookMark Kepper/script.js
+++ Day 10 - BookMark Kepper/script.js
@@ -78,8 +78,8 @@ function fetchBookmarks() {
     // Create bookmarks array in localStorage
     bookmarks = [
       {
-        name: 'Jacinto Design',
-        url: 'http://jacinto.design',
+        name: 'Google',
+        url: 'https://www.google.com/',
       },
     ];
     localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
